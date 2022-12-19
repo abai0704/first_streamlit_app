d453465110a8bc9dbe5b1e1362021c89f835c014
abai0704 <abai0704@gmail.com>
2022-12-19 11:21:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title("My Parents New Healthy Diner")
+streamlit.title('My Parents New Healthy Diner')
+
+steamlit.header('Breakfast Menu')
+steamlit.text('Omega 3 & Blueberry Oatmeal')
+steamlit.text('Kale, Spinach & Rocket Smootie')
+steamlit.text('Hard-Boiled Free-Range Egg')
+
